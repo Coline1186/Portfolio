5ae893e7a5a113c7da40cb7cd45c8bd5e72a295d
Coline1186 <coline.grosso@neuf.fr>
2024-07-02 00:23:57 +0200

    responsive almost done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,33 @@
 import { HashLink as Link } from "react-router-hash-link";
-// import { BsMoonStarsFill, BsFillSunFill } from "react-icons/bs";
 import "./Header.css";
 import "animate.css";
 import moi from "../../assets/IMG_5476.jpeg";
 import MenuBurger from "../MenuBurger/MenuBurger";
+import { useState } from "react";
+
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const scrollWithOffset = (el, offset) => {
     const elementPosition = el.offsetTop - offset;
     window.scroll({
       top: elementPosition,
       left: 0,
-      behavior: "smooth", 
+      behavior: "smooth",
     });
+  };
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
 
   return (
     <header id="header">
-      <nav style={navStyle}>
+      <nav className="navStyle">
         <Link
           to="/#home"
-          style={linkStyle}
+          className="linkStyle link-image"
           scroll={(el) => scrollWithOffset(el, 100)}
-          className="link-image"
         >
           <img
             src={moi}
@@ -29,57 +35,44 @@ function Header() {
             className="animate__animated animate__backInDown"
           />
         </Link>
-        <Link
-          to="/#about"
-          scroll={(el) => scrollWithOffset(el, 100)}
-          style={linkStyle}
-          className=" animate__animated animate__backInDown"
-        >
-          A propos
-        </Link>
-        <Link
-          to="/#skills"
-          scroll={(el) => scrollWithOffset(el, 100)}
-          style={linkStyle}
-          className=" animate__animated animate__backInDown"
-        >
-          Mes compétences
-        </Link>
-        <Link
-          to="/#projects"
-          scroll={(el) => scrollWithOffset(el, 100)}
-          style={linkStyle}
-          className=" animate__animated animate__backInDown"
-        >
-          Mes Projets
-        </Link>
-        <Link
-          to="/#contact"
-          scroll={(el) => scrollWithOffset(el, 100)}
-          style={linkStyle}
-          className=" animate__animated animate__backInDown"
-        >
-          Contact
-        </Link>
-        <MenuBurger />
+        <div className={`nav-links ${isMenuOpen ? "open" : ""}`}>
+          <Link
+            to="/#about"
+            className="linkStyle animate__animated animate__backInDown"
+            scroll={(el) => scrollWithOffset(el, 100)}
+            onClick={toggleMenu}
+          >
+            A propos
+          </Link>
+          <Link
+            to="/#skills"
+            className="linkStyle animate__animated animate__backInDown"
+            scroll={(el) => scrollWithOffset(el, 100)}
+            onClick={toggleMenu}
+          >
+            Mes compétences
+          </Link>
+          <Link
+            to="/#projects"
+            className="linkStyle animate__animated animate__backInDown"
+            scroll={(el) => scrollWithOffset(el, 100)}
+            onClick={toggleMenu}
+          >
+            Mes Projets
+          </Link>
+          <Link
+            to="/#contact"
+            className="linkStyle animate__animated animate__backInDown"
+            scroll={(el) => scrollWithOffset(el, 100)}
+            onClick={toggleMenu}
+          >
+            Contact
+          </Link>
+        </div>
+        <MenuBurger toggleMenu={toggleMenu} isMenuOpen={isMenuOpen} />
       </nav>
     </header>
   );
 }
-
-const navStyle = {
-  display: "flex",
-  justifyContent: "space-around",
-  alignItems: "center",
-  height: "100px",
-};
-
-const linkStyle = {
-  color: "var(--primary-color)",
-  textDecoration: "none",
-  fontSize: "1.2rem",
-  fontFamily: "var(--primary-font)",
-  fontWeight: "600",
-};
 
 export default Header;
--- src/components/MenuBurger/MenuBurger.jsx
+++ src/components/MenuBurger/MenuBurger.jsx
@@ -1,59 +1,12 @@
-/* eslint-disable react/no-unescaped-entities */
-import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 import "./MenuBurger.css";
-import { Link } from "react-router-dom";
 
-function MenuBurger() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleOpen = () => {
-    setIsOpen(true);
-  };
-  
-    return (
-      <div id="menuBurger">
-        <div className={`burgermenu ${isOpen ? "show-burger" : "hide-burger"}`}>
-          <ul className="burgermenu-links">
-            <li className="burgermenu-items">
-              <Link to="/" className="burgermenu-link">
-                Page d'entrée
-              </Link>
-            </li>
-            <li
-              className="burgermenu-items"
-              role="presentation"
-              onClick={handleOpen}
-              onKeyDown={handleOpen}
-            >
-              <a href="#link-burger" className="burgermenu-link">
-                Comment ça marche ?
-              </a>
-            </li>
-            <li className="burgermenu-items">
-              <Link to="/faq" className="burgermenu-link">
-                FAQ
-              </Link>
-            </li>
-          </ul>
-          <button
-            aria-label="button"
-            type="button"
-            className="burger-bar"
-            onClick={handleOpen}
-            onKeyDown={handleOpen}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="48"
-              viewBox="0 -960 960 960"
-              width="48"
-            >
-              <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
-            </svg>
-          </button>
-        </div>
-      </div>
-    );
+function MenuBurger({ toggleMenu, isMenuOpen }) {
+  return (
+    <div className="menu-burger" onClick={toggleMenu}>
+      {isMenuOpen ? <FaTimes size={30} /> : <FaBars size={30} />}
+    </div>
+  );
 }
 
 export default MenuBurger;
